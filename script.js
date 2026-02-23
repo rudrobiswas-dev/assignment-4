c9808de124e78efd1becb80e201658d7db6aa1b0
rudrobiswas-dev <rudrobiswas11023@gmail.com>
2026-02-23 15:58:36 +0600

    html 100 and style 70 done now working on js

--- script.js
+++ script.js
@@ -84,5 +84,90 @@ const jobsData = [
 let currentTab = "all";
 
 function updateDashboard() {
-  
+  document.getElementById("totalCount").innerText = jobsData.length;
+  document.getElementById("interviewCount").innerText =
+    jobsData.filter(j => j.status === "interview").length;
+  document.getElementById("rejectedCount").innerText =
+    jobsData.filter(j => j.status === "rejected").length;
 }
+
+function renderJobs() {
+  const container = document.getElementById("jobsContainer");
+  container.innerHTML = "";
+
+  let filtered = jobsData.filter(job => {
+    if(currentTab === "all") return true;
+    return job.status === currentTab;
+  });
+
+  document.getElementById("tabCount").innerText = filtered.length + " Jobs";
+
+  if(filtered.length === 0){
+    container.innerHTML = `
+      <div class="empty">
+        <img src="./jobs.png">
+        <h3>No jobs available</h3>
+        <p>Check back soon for new job opportunities.</p>
+      </div>
+    `;
+    return;
+  }
+
+  filtered.forEach(job => {
+    const card = document.createElement("div");
+    card.className = "job-card";
+    card.innerHTML = `
+        <div class="h3-and-icon">
+            <div>
+              <h3>${job.position}</h3> 
+              <p><strong>${job.companyName}</strong></p>
+            </div>
+            <div class="icon">
+              <i class="fa-regular fa-trash-can delete"></i>
+            </div>
+        </div>
+        <br/>
+        <p>${job.location} . ${job.type} . ${job.salary}</p>
+
+        <h6 class="card-description">${job.description}</h6>
+
+        <div class="buttons">
+          <button class="interview">Interview</button>
+          <button class="rejected">Rejected</button>
+        </div>
+        `;
+
+    card.querySelector(".interview").addEventListener("click", () => {
+  job.status = "interview";
+  updateDashboard();
+  renderJobs();
+});
+
+   card.querySelector(".rejected").addEventListener("click", () => {
+  job.status = "rejected";
+  updateDashboard();
+  renderJobs();
+});
+
+    card.querySelector(".delete").onclick = () => {
+      const index = jobsData.findIndex(j => j.id === job.id);
+      jobsData.splice(index,1);
+      updateDashboard();
+      renderJobs();
+    };
+
+    container.appendChild(card);
+  });
+}
+
+document.querySelectorAll(".tab").forEach(tab=>{
+  tab.onclick = () => {
+    document.querySelectorAll(".tab").forEach(t=>t.classList.remove("active"));
+    tab.classList.add("active");
+    currentTab = tab.dataset.tab;
+    renderJobs();
+  }
+});
+
+updateDashboard();
+renderJobs();
